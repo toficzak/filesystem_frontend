658c0580279d13aa936e99cf8907d01725d96a73
Krzysztof Kucharczyk <krzysztof.kucharczyk@hotmail.com>
2019-09-09 21:37:57 +0200

    Href'ed

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -3,10 +3,11 @@ import Link from '@material-ui/core/Link';
 
 
 class Breadcrumb extends Component {
-
     render(){
+        const hrefValue = 'localhost:3000/files/' + this.props.breadcrumb.id
+
         return (
-            <Link color="inherit" href="/">
+            <Link color="inherit" href={hrefValue} >
                 {this.props.breadcrumb.name}
             </Link>
         ) 
